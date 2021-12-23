0323fd52b54abb89ebb873f0cf305250f2ffaf60
abdelghani ameni <ameni.abdelghani@gmail.com>
2021-12-23 18:25:12 +0100

    update menu item withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/homepages/homepage.component';
+
+import HomePage from './pages/homepages/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGES </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-   <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepages/homepage.component.jsx
+++ src/pages/homepages/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React  from "react";
 import Directory from "../../components/directory/directory.component";
 import './homepage.styles.scss';
-export default function Homepage() {
+function Homepage() {
     return (
         <div className='homepage'>
           <Directory/>
         </div>
     );
 }
+export default Homepage;
